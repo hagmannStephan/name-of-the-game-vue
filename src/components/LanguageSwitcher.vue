f8ae4048c6d1527bd9c26222f145bc9feff09492
MrLulmer <liamulmer24@gmail.com>
2025-07-14 12:51:37 +0000

    Changed Language Switch

--- src/components/LanguageSwitcher.vue
+++ src/components/LanguageSwitcher.vue
@@ -1,24 +1,24 @@
 <template>
-    <button @click="toggleLanguage">
-      {{ currentLocale === 'de' ? 'English' : 'Deutsch (CH)' }}
-    </button>
-  </template>
+  <div class="toggle" :class="{ right: currentLocale === 'en' }" @click="toggleLanguage">
+    <div class="highlight"></div>
+    <div class="option">Deutsch</div>
+    <div class="option">English</div>
+  </div>
+</template>
   
-  <script setup lang="ts">
+<script setup lang="ts">
   import { computed } from 'vue'
   import { useI18n } from 'vue-i18n'
   import { useLanguageSettingsStore } from '@/stores/languageSettingsStore'
-  
+
   const { locale } = useI18n()
   const languageSettingsStore = useLanguageSettingsStore()
-  
-  // Use a computed to reactively track the current locale
+
   const currentLocale = computed(() => locale.value)
-  
+
   function toggleLanguage() {
     const newLocale = locale.value === 'de' ? 'en' : 'de'
     locale.value = newLocale
     languageSettingsStore.setLanguage(newLocale)
   }
-  </script>
-  
+</script>
